d09c9a36a4a4c05ea0887d55a89518cc77882d37
TallulahDrn <tallulah.dreno44@gmail.com>
2024-05-19 17:29:21 +0200

    reload

--- js/accueil.js
+++ js/accueil.js
@@ -1,1 +1,18 @@
 //page accueil
+function loader(){
+    //cacher le body et après le montrer en modifiant css avec js
+    let load = document.getElementById("reload");
+    console.log(load);
+    load.style.display = 'none';
+    let page = document.getElementById("page");
+    page.style.display = 'flex';
+}
+
+function load(){
+    setTimeout(loader,2000);
+}
+function main(){
+    load();
+}
+main();
+//prb horloge : met pas l'heure de l'aprem
