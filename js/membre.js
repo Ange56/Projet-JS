bc6b89bbb189361103a4669a00666b2b43eb89a9
TallulahDrn <tallulah.dreno44@gmail.com>
2024-05-25 23:01:49 +0200

    membres

--- js/membre.js
+++ js/membre.js
@@ -1,1 +1,24 @@
 //membre
+
+
+/*---------------------IMAGE A GRATTER---------------------*/
+function gratter(){
+    let contact = document.getElementsByName("karine")[0];
+    //let image = new Image();
+    document.getElementsByName("karine")[0]= "../images/contact.jpg";
+    //image.style.position = "relative";
+
+    //contact.appendChild(image);
+
+
+}
+
+
+
+function main(){
+    gratter();
+
+}
+
+main();
+//image à gratter en javascript
