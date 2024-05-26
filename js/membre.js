9382b923d4e3c76659d9095dc4a63f0ebec8dc1e
TallulahDrn <tallulah.dreno44@gmail.com>
2024-05-26 21:19:24 +0200

    carte a gratter

--- js/membre.js
+++ js/membre.js
@@ -2,23 +2,63 @@
 
 
 /*---------------------IMAGE A GRATTER---------------------*/
-function gratter(){
+
+
+
+function grattage(){
     let contact = document.getElementsByName("karine")[0];
-    //let image = new Image();
-    document.getElementsByName("karine")[0]= "../images/contact.jpg";
-    //image.style.position = "relative";
+    let canva = document.createElement("canvas");
+    canva.classList.add("gratter");
+    contact.appendChild(canva);
+    let photo = canva.getContext("2d");
+    contact = contact.getElementsByTagName("img")[0];
+    canva.width = contact.offsetHeight;
+    console.log(canva);
+    console.log(canva.width);
+    canva.height = canva.width;
 
-    //contact.appendChild(image);
+    let imageDessus = new Image();
+    imageDessus.src = "../images/contact.jpg";
 
 
+    imageDessus.onload = () => {
+        photo.drawImage(imageDessus,0,0,canva.width,canva.height);
+        photo.globalCompositeOperation = 'destination-out';
+    };
+    console.log(imageDessus);
+
+
+    let gratte = false;
+    let x;
+    let y;
+
+    let cadre = canva.getBoundingClientRect();
+
+    canva.addEventListener('mousedown', (souris) => {
+        gratte = true;
+    });
+    document.addEventListener('mouseup', (souris) => {
+        gratte = false;
+    });
+
+    canva.addEventListener('mousemove', (souris) => {
+        if(gratte){
+            x = souris.clientX - cadre.left;
+            y = souris.clientY - cadre.top;
+
+            // Draw a circle to create the scratch effect
+            photo.beginPath();
+            photo.arc(x, y, 13, 0, 180);
+            photo.fill();
+        }
+    });
 }
 
 
 
 function main(){
-    gratter();
+    grattage();
 
 }
 
 main();
-//image à gratter en javascript
