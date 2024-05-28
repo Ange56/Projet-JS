27971874204d2134691c408fe0edaba81a67db95
TallulahDrn <tallulah.dreno44@gmail.com>
2024-05-28 22:37:28 +0200

    membres edition v1

--- js/membre.js
+++ js/membre.js
@@ -13,8 +13,6 @@ function grattage(){
     let photo = canva.getContext("2d");
     contact = contact.getElementsByTagName("img")[0];
     canva.width = contact.offsetHeight;
-    console.log(canva);
-    console.log(canva.width);
     canva.height = canva.width;
 
     let imageDessus = new Image();
@@ -25,7 +23,6 @@ function grattage(){
         photo.drawImage(imageDessus,0,0,canva.width,canva.height);
         photo.globalCompositeOperation = 'destination-out';
     };
-    console.log(imageDessus);
 
 
     let gratte = false;
@@ -56,9 +53,69 @@ function grattage(){
 
 
 
+
+
+/*----------------------MODE EDITION-------------------------*/
+function edition_appui_bouton(){
+    let bouton_edition = document.getElementById("bouton-edition");
+    let mode_edition = window.prompt("Entrez le nom du profil administrateur","nom");
+    if(mode_edition == "admin"){    
+        let new_window = window.prompt("Entrez le mot de passe du profil administrateur", "mot de passe");
+        if(new_window =="admin_pwd"){
+            if (bouton_edition.style.backgroundColor === "black"){
+                bouton_edition.style.backgroundColor = "#CA0000"; // couleur originale
+                bouton_edition.style.borderColor = "black"; // couleur originale
+            } 
+            else{
+                bouton_edition.style.backgroundColor = "black"; // nouvelle couleur
+                bouton_edition.style.borderColor = "red"; // nouvelle couleur
+                nouvelle_carte();
+            }
+        }
+    }
+}
+function nouvelle_carte(){
+    let page = document.getElementById("page");
+    let card2 = document.getElementById("c");
+    let plus = document.createElement("img");
+    let new_card = document.createElement("div");
+
+    
+    page.insertBefore(plus, card2);
+    plus.src = "../images/plus.png";
+    plus.height = "100";
+    plus.width ="100";
+    //le centrer
+    //rajouter icones crayons sinon
+    
+    page.style.gridTemplateColumns = "1fr 5fr 1fr 5fr 1fr 5fr 1fr";
+    plus.style.gridRowStart ="3";/*commence à la deuxième ligne*/
+    plus.style.gridRowEnd="4";/*finit à la 3e ligne*/
+    plus.style.gridColumnStart= "6";/*commence à la 2e colonne*/
+    plus.style.gridColumnEnd= "7";
+    
+    plus.addEventListener("click", () =>{
+        plus.style.display = "none";
+        new_card.classList.add("card");
+        new_card.id="cbis";
+        new_card.style.gridRowStart ="3";/*commence à la deuxième ligne*/
+        new_card.style.gridRowEnd="4";/*finit à la 3e ligne*/
+        new_card.style.gridColumnStart= "6";/*commence à la 2e colonne*/
+        new_card.style.gridColumnEnd= "7";
+    });
+
+
+
+    page.insertBefore(new_card, card2);
+
+}
+
+
+
 function main(){
     grattage();
 
 }
 
 main();
+//prb hover avec js
