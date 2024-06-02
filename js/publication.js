66c6b56337f5763221bd4d635f0700c6cfec81be
asma-abc <148458412+asma-abc@users.noreply.github.com>
2024-06-02 14:44:49 +0200

    icone flèche

--- js/publication.js
+++ js/publication.js
@@ -1,6 +1,59 @@
-//publication
+function filterFunction() {
+    // Get the user input for name title and year
+    let nameInput = document.getElementById("name").value.toLowerCase();
+    let titleInput = document.getElementById("titre").value.toLowerCase();
+    let yearInput = document.getElementById("annee").value.toLowerCase();
+    var com = document.getElementById("com").checked;
+    var article = document.getElementById("article").checked;
+    alert(article);
+    
+  /*
+    // Get all <p> tags
+    const paragraphs = document.getElementsByTagName("p");
+  
+    // Loop through each paragraph
+    for (let i = 0; i < paragraphs.length; i++) {
+        const paragraphText = paragraphs[i].textContent.toLowerCase();
+  
+        // Check if the paragraph contains any of the user inputs
+        let nameMatch = nameInput.length > 0 && paragraphText.includes(nameInput);
+        let titleMatch = titleInput.length > 0 && paragraphText.includes(titleInput);
+        let yearMatch =yearInput.length > 0 &&  paragraphText.includes(yearInput);
+        
+        // Show the paragraph if any of the inputs match
+        if (nameMatch || titleMatch || yearMatch) {
+            //alert(article);
+            
+            /*if (article == 1 && paragraphs[i].id.includes('revu')){
+                paragraphs[i].style.display = "block"; // Show the matching paragraph
+            }
+            else if (com == 1 && paragraphs[i].id.includes('com')){
+                paragraphs[i].style.display = "block";  
+            }
+            else {
+                paragraphs[i].style.display = "block";
+            }
+        }
+        else{
+            paragraphs[i].style.display = "none"; // Hide non-matching paragraphs
+
+        }
+         
+            
+        
+        
+    }*/
+
+  
+  // Attach the filterFunction to the form's submit event
+    document.querySelector(".filter").addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent form submission
+        filterFunction(); // Call the filter function
+    });
+}
 
 
+/*
 function filterFunction() {
     // Get the user input for name title and year
     let nameInput = document.getElementById("name").value.toLowerCase();
@@ -18,7 +71,7 @@ function filterFunction() {
         let nameMatch = nameInput.length > 0 && paragraphText.includes(nameInput);
         let titleMatch = titleInput.length > 0 && paragraphText.includes(titleInput);
         let yearMatch =yearInput.length > 0 &&  paragraphText.includes(yearInput);
-  
+        
         // Show the paragraph if any of the inputs match
         if (nameMatch || titleMatch || yearMatch) {
             paragraphs[i].style.display = "block"; // Show the matching paragraph
